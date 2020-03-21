b518ed975e8dad953112c86d178cbbad5c6b53e4
namwoo.kim <sky_9233@naver.com>
2020-03-21 13:59:42 +0900

    kalink

--- main.js
+++ main.js
@@ -131,41 +131,45 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
     }
     //테스트
     if (msg == ".테") {
-        Kakao.send(room,
-            {
-                "link_ver": "4.0",
-                "template_object":
+        try{
+            Kakao.send(room,
                 {
-                    "object_type": "feed",
-                    "button_title": "버튼",
-
-                    "content": {
-                        "title": "제목",
-                        "image_url": "http://k.kakaocdn.net/dn/dEwroC/btqgdYG36hU/1zQbPe8ZLpGGmoMkrf0iX0/kakaolink40_original.png",
-                        "link":
-                        {
-                            "web_url": "naver.com",
-                            "mobile_web_url": "naver.com"
-                        },
-                        "description": "설명"
-                    },
-
-                    "buttons": [
-                        {
-                            "title": "버튼",
+                    "link_ver": "4.0",
+                    "template_object":
+                    {
+                        "object_type": "feed",
+                        "button_title": "버튼",
+    
+                        "content": {
+                            "title": "제목",
+                            "image_url": "http://k.kakaocdn.net/dn/dEwroC/btqgdYG36hU/1zQbPe8ZLpGGmoMkrf0iX0/kakaolink40_original.png",
                             "link":
                             {
-                                "web_url": "noirstar.tistory.com",
-                                "mobile_web_url": "noirstar.tistory.com"
+                                "web_url": "naver.com",
+                                "mobile_web_url": "naver.com"
+                            },
+                            "description": "설명"
+                        },
+    
+                        "buttons": [
+                            {
+                                "title": "버튼",
+                                "link":
+                                {
+                                    "web_url": "noirstar.tistory.com",
+                                    "mobile_web_url": "noirstar.tistory.com"
+                                }
                             }
-                        }
-                    ]
-
-                }
-            });
+                        ]
+    
+                    }
+                });
+        }catch(e) {
+            replier.reply(e);
+        }    
     }
     if (msg == ".테2") {
-        replier.reply(randomM.test2());
+        replier.reply(room);
         return;
     }
     //네이버 파파고
